efd53e071e1312647ce151d1c269ce127e7240e6
hongntb <bichhongbxvp@gmail.com>
2023-01-12 15:34:20 +0700

    Initial files

---
+++ DatingApp/DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetAllUsers()
+        {
+            var users = _context.Users.ToList();
+            return Ok(users);
+        }
+
+        [HttpGet]
+        public ActionResult<AppUser> GetUser(Guid id)
+        {
+            var user = _context.Users.Find(id);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+    }
+}
